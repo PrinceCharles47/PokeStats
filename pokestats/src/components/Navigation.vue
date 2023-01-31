ad388b9af0c46850043fe64e7dd9869a9dc62654
PrinceCharles47 <clemente.pcharles@gmail.com>
2023-01-31 23:47:17 +0700

    created a navigation component

---
+++ pokestats/src/components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-container>
+      <v-app-bar
+        app
+        dense
+        id="nav-bar"
+      >
+        <v-app-bar-nav-icon @click="drawer = !drawer" dark></v-app-bar-nav-icon>
+        <span class="white--text font-weight-bold">PokeSTATS</span>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn
+          href="https://anime-scene-tracer.pages.dev/"
+          target="_blank"
+          plain
+          class="px-0"
+        >
+          <span class="white--text">Also try AniTRACE</span>
+        </v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer absolute temporary v-model="drawer"></v-navigation-drawer>
+    </v-container>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'App',
+  
+    data: () => ({
+      drawer: false
+    }),
+  };
+  </script>
+  
+  <style scoped>
+
+  #nav-bar{
+    background-color: transparent;
+  }
+  
+  </style>
+  
